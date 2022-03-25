60a8962ae6cd3d1c860f65997ded1012a98005a2
MateusQuintanilha <100220698+MateusQuintanilha@users.noreply.github.com>
2022-03-25 20:32:16 -0300

    adicionando conteúdo

--- web-development/javascript/praticando/cronometro/assets/js/main.js
+++ web-development/javascript/praticando/cronometro/assets/js/main.js
@@ -1,1 +1,34 @@
 
+function getTimeForSeconds(segundos) {
+    const milissegundos = segundos * 1000;
+    const data = new Date(milissegundos);
+    return data.toLocaleTimeString('pt-BR', {
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+        hour12: false,
+        timeZone: 'GMT'
+    });
+}
+
+const cronometro = document.getElementById('timer');
+const buttonIniciar = document.getElementById('iniciar');
+const buttonPausar = document.getElementById('pausar');
+const buttonZerar = document.getElementById('zerar');
+let segundos = 0;
+let timer;
+
+function iniciarCronometro() {
+    timer = setInterval(() => {
+        segundos++;
+        cronometro.innerText = getTimeForSeconds(segundos);
+    }, 1000);
+}
+
+buttonIniciar.addEventListener('click', () => iniciarCronometro());
+buttonPausar.addEventListener('click', () => clearInterval(timer));
+buttonZerar.addEventListener('click', () => {
+    clearInterval(timer);
+    segundos = 0;
+    cronometro.innerText = getTimeForSeconds(segundos);
+});
