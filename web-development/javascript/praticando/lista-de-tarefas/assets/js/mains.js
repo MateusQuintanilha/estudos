cb708335f83345da70e02995e404e9ed6e0884b8
MateusQuintanilha <100220698+MateusQuintanilha@users.noreply.github.com>
2022-03-27 19:30:58 -0300

    adicionando funcionlidade de adicionar e salvar tarefa no localStorage

--- web-development/javascript/praticando/lista-de-tarefas/assets/js/mains.js
+++ web-development/javascript/praticando/lista-de-tarefas/assets/js/mains.js
@@ -3,5 +3,31 @@ const btnAdicionar = document.querySelector('.btn-adicionar');
 const tarefas = document.querySelector('.tarefas');
 
 function criaTarefa(conteudoTarefa) {
-    const tarefaCompleta
+    const tarefaCompleta = ˋ
+    <tr class="tarefa">
+        <td class="tarefa-texto">${conteudoTarefa}</td>
+        <td><button class="btn-apagar">Apagar</button></td>
+    </tr>
+    ˋ;
+    limpaInput();
+    return tarefaCompleta;
+}
+function limpaInput() {
+    inputTarefa.value = '';
+    inputTarefa.focus();
+}
+function adicionaTarefa(tarefaCompleta) {
+    tarefas.innerHTML += tarefaCompleta;
+}
+function salvaTarefa() {
+    const tarefas = document.querySelectorAll('.tarefa');
+    const listaDeTarefas = [];
+
+    for (const tarefa of tarefas) {
+        const tarefaCompleta = tarefa.querySelector('.tarefa-texto');
+        const textoTarefa = tarefaCompleta.innerText;
+        listaDeTarefas.push(textoTarefa);
+    }
+    const tarefasJSON = JSON.stringify(listaDeTarefas);
+    localStorage.setItem('tarefas', tarefasJSON);
 }
