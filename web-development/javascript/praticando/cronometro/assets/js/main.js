582c84bce3318694ce4d0e06498843f02650a9f4
MateusQuintanilha <100220698+MateusQuintanilha@users.noreply.github.com>
2022-03-26 22:02:48 -0300

    atulizando o código

--- web-development/javascript/praticando/cronometro/assets/js/main.js
+++ web-development/javascript/praticando/cronometro/assets/js/main.js
@@ -1,34 +1,43 @@
+(function() {
+    const cronometro = document.getElementById('timer');
+    let segundos = 0;
+    let timer;
 
-function getTimeForSeconds(segundos) {
-    const milissegundos = segundos * 1000;
-    const data = new Date(milissegundos);
-    return data.toLocaleTimeString('pt-BR', {
-        hour: '2-digit',
-        minute: '2-digit',
-        second: '2-digit',
-        hour12: false,
-        timeZone: 'GMT'
+    function pegarSegundosDoTempo(segundos) {
+        const milissegundos = segundos * 1000;
+        const data = new Date(milissegundos);
+        return data.toLocaleTimeString('pt-BR', {
+            hour: '2-digit',
+            minute: '2-digit',
+            second: '2-digit',
+            hour12: false,
+            timeZone: 'GMT'
+        });
+    }
+
+    function iniciarCronometro() {
+        timer = setInterval(() => {
+            segundos++;
+            cronometro.innerText = pegarSegundosDoTempo(segundos);
+        }, 1000);
+    }
+
+    document.addEventListener('click', (event) => {
+        const elemento = event.target;
+
+        if(elemento.classList.contains('iniciar')){
+            iniciarCronometro();
+            cronometro.classList.remove('pausado');
+        }
+        if(elemento.classList.contains('pausar')){
+            clearInterval(timer);
+            cronometro.classList.toggle('pausado');
+        }
+        if(elemento.classList.contains('zerar')){
+            clearInterval(timer);
+            segundos = 0;
+            cronometro.innerText = pegarSegundosDoTempo(segundos);
+            cronometro.classList.remove('pausado');
+        }
     });
-}
-
-const cronometro = document.getElementById('timer');
-const buttonIniciar = document.getElementById('iniciar');
-const buttonPausar = document.getElementById('pausar');
-const buttonZerar = document.getElementById('zerar');
-let segundos = 0;
-let timer;
-
-function iniciarCronometro() {
-    timer = setInterval(() => {
-        segundos++;
-        cronometro.innerText = getTimeForSeconds(segundos);
-    }, 1000);
-}
-
-buttonIniciar.addEventListener('click', () => iniciarCronometro());
-buttonPausar.addEventListener('click', () => clearInterval(timer));
-buttonZerar.addEventListener('click', () => {
-    clearInterval(timer);
-    segundos = 0;
-    cronometro.innerText = getTimeForSeconds(segundos);
-});
+})();
