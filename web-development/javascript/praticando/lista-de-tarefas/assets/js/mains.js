552c63e35b7154f89608956a1fe7c5f04680bf00
MateusQuintanilha <100220698+MateusQuintanilha@users.noreply.github.com>
2022-03-27 19:01:22 -0300

    Pegando as informações do HTML

---
+++ web-development/javascript/praticando/lista-de-tarefas/assets/js/mains.js
@@ -0,0 +1,7 @@
+const inputTarefa = document.querySelector('.input-tarefa');
+const btnAdicionar = document.querySelector('.btn-adicionar');
+const tarefas = document.querySelector('.tarefas');
+
+function criaTarefa(conteudoTarefa) {
+    const tarefaCompleta
+}
