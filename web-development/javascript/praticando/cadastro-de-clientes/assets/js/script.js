29a4416c1a170064a6657f1919b1c73334cc89ef
MateusQuintanilha <100220698+MateusQuintanilha@users.noreply.github.com>
2022-06-04 20:39:13 -0300

    adicionando localStorage

--- web-development/javascript/praticando/cadastro-de-clientes/assets/js/script.js
+++ web-development/javascript/praticando/cadastro-de-clientes/assets/js/script.js
@@ -4,6 +4,11 @@ const closeModal = () => {
     document.getElementById('modal').classList.remove('active')
 };
 
+// localStorage
+const nameDataBase = 'db_client';
+const getLocalStorage = () => JSON.parse(localStorage.getItem(nameDataBase)) ?? [];
+const setLocalStorage = (contentDB) => localStorage.setItem(nameDataBase, JSON.stringify(contentDB));
+
 // Events
 document.getElementById('new-client')
     .addEventListener('click', openModal);
