14d773792837020d8c78003c005db82db5aad60f
MateusQuintanilha <100220698+MateusQuintanilha@users.noreply.github.com>
2022-06-09 19:57:22 -0300

    Criando CRUD

--- web-development/javascript/praticando/cadastro-books/assets/js/script.js
+++ web-development/javascript/praticando/cadastro-books/assets/js/script.js
@@ -11,3 +11,24 @@ function Book() {
 const nameDB = 'book_db';
 const getLocalStorage = () => JSON.parse(localStorage.getItem(nameDB)) ?? [];
 const setLocalStorage = (bookDB) => localStorage.setItem(nameDB, JSON.stringify(bookDB));
+
+// CRUD
+const createRegisterBook = (book) => {
+    const bookDB = getLocalStorage();
+    bookDB.push(book);
+    setLocalStorage(bookDB);
+}
+
+const readRegisterBook = () => JSON.parse(localStorage.getItem(nameDB));
+
+const updateRegisterBook = (index, book) => {
+    const bookDB = getLocalStorage();
+    bookDB[index] = book;
+    setLocalStorage(bookDB);
+}
+
+const deleteRegisterBook = (index) => {
+    const bookDB = getLocalStorage();
+    bookDB.splice(index, 1);
+    setLocalStorage(bookDB);
+}
