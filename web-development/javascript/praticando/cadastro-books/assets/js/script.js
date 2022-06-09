b2c9d5950a5c6a046e86d671a1f995dca5e31f9f
MateusQuintanilha <100220698+MateusQuintanilha@users.noreply.github.com>
2022-06-09 20:01:54 -0300

    Criando as funções de ações como salvar e exluir um livro

--- web-development/javascript/praticando/cadastro-books/assets/js/script.js
+++ web-development/javascript/praticando/cadastro-books/assets/js/script.js
@@ -32,3 +32,51 @@ const deleteRegisterBook = (index) => {
     bookDB.splice(index, 1);
     setLocalStorage(bookDB);
 }
+
+// Actions
+const createTableRow = (book, index, tbody) => {
+    const tr = document.createElement('tr');
+    tr.innerHTML = `
+        <td>${book.title}</td>
+        <td>${book.author}</td>
+        <td>${book.language}</td>
+        <td>
+            <button type="button" id="edit-${index}">Editar</button>
+            <button type="button" id="delete-${index}">Deletar</button>
+        </td>
+    `;
+    tbody.appendChild(tr);
+};
+
+const updateTable = () => {
+    const tbody = document.getElementById('book-tbody');
+    tbody.innerHTML = '';
+
+    const bookDB = getLocalStorage();
+    bookDB.forEach((book, index) => createTableRow(book,index, tbody));
+};
+
+const clearFields = () => document.getElementById('book-register').reset();
+
+const saveBook = () => {
+    const isValidFields = document.getElementById('book-register').reportValidity();
+    if (!isValidFields) return;
+    createRegisterBook(Book());
+    clearFields();
+    updateTable();
+};
+
+const editField = (event) => {
+    const element = event.target;
+    if(element.type !== 'button') return;
+
+    const [action, index] = element.id.split('-');
+    if(action === 'edit') console.log('editar');
+    if(action === 'delete') {
+        const bookDB = getLocalStorage();
+        const response = confirm(`Você realmente deseja excluir o livro: ${bookDB[index].title}?`);
+        if(!response) return;
+        deleteRegisterBook(index);
+        updateTable();
+    }
+};
